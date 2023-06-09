099fed15c4b70ef1d3d3dc662187ea4a9b72b3db
Ziad Ayman <losziad20189@gmail.com>
2023-06-08 19:27:52 -0700

    Finish From Confetti Package, It's Something Like Party

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/UI/neu_button/neu_button.dart';
 
@@ -9,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  //FOR NEU BUTTON
   bool isButtonPressed = false;
   void buttonPressed(){
     setState(() {
@@ -24,16 +27,47 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  //FOR CONFETTI PACKAGE
+  final controller = ConfettiController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+  bool isPlaying = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Center(
-        child: NeuButton(
-          onTap: buttonPressed,
-          isButtonPressed: isButtonPressed,
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Scaffold(
+         appBar: AppBar(),
+          body: Center(
+            child: MaterialButton(
+              onPressed: (){
+                if(isPlaying)
+                {
+                  controller.stop();
+                }
+                else
+                {
+                  controller.play();
+
+                }
+                isPlaying = !isPlaying;
+              },
+              child: Text('Party Time'),
+              color: Colors.deepPurple[200],
+            ),
+          ),
         ),
-      ),
+        ConfettiWidget(
+          confettiController: controller,
+        blastDirection: pi / 2,
+        ),
+      ],
     );
   }
 }
